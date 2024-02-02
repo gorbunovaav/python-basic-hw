61070b8a87929e16b6e117f6374848246cce52ff
Anna Gorbunova <81052418+gorbunovaav@users.noreply.github.com>
2024-02-02 19:12:39 +0300

    final hw version with mistakes

--- homework_02/car.py
+++ homework_02/car.py
@@ -6,11 +6,10 @@ class Car(base.Vehicle):
     def __init__(self, engine):
         self.engine = engine
 
-    a = eng.Engine(0, 0)
+    # a = eng.Engine(0, 0)
     #
-
-    def set_engine(self, a):
-        a = Car()
+    # def set_engine(self, a):
+    #     a = Car()
 
 
 
