8f29a23fa0b0caaad97821b3f3b75a1566ccfbee
Anna Gorbunova <81052418+gorbunovaav@users.noreply.github.com>
2024-11-09 22:17:42 +0300

    HW4 list import added

--- homework_04/models.py
+++ homework_04/models.py
@@ -15,7 +15,7 @@ from sqlalchemy.ext.asyncio import(
     AsyncSession
 )
 from sqlalchemy.orm import DeclarativeBase, declared_attr, Mapped, mapped_column
-from sqlalchemy import Integer, String, ForeignKey, MetaData, relationship
+from sqlalchemy import Integer, String, ForeignKey, MetaData, relationship, List
 
 PG_CONN_URI = os.environ.get("SQLALCHEMY_PG_CONN_URI") or "postgresql+asyncpg://postgres:password@localhost/postgres"
 
