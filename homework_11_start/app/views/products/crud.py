bb0424caee0d054cbd745918955c0ca22e62cbbc
Anna Gorbunova <81052418+gorbunovaav@users.noreply.github.com>
2024-12-01 14:59:53 +0300

    hw 11 done

---
+++ homework_11_start/app/views/products/crud.py
@@ -0,0 +1,34 @@
+
+from random import randint
+
+from sqlalchemy import select, exists, func
+
+from models import db, Product
+
+
+class ProductsStorage:
+
+    def create(self, name: str, price: int | None = None) -> Product:
+        product = Product(
+            name=name,
+            price=price or randint(10, 1000),
+        )
+        db.session.add(product)
+        db.session.commit()
+        return product
+
+    def get(self) -> list[Product]:
+        results = db.session.scalars(select(Product).order_by("id")).all()
+        return list(results)
+
+    def get_by_id(self, product_id: int) -> Product | None:
+        return db.session.get(Product, ident=product_id)
+
+    # def name_exists(self, product_name: str) -> bool:
+    #     stmt = exists().where(
+    #         func.lower(Product.name) == product_name.lower(),
+    #     )
+    #     return db.session.scalar(select(stmt))
+
+
+products_storage = ProductsStorage()
